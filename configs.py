702e1b73abd1960c65bdb5f7bf677ebe7868993c
youdi1012 <1379263747@qq.com>
2022-03-01 20:52:37 +0800

    纠正前端部分错误

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
-PROJECT_PATH = "E:\\大创推荐算法\\cfaProj"
-DATA_PATH = "E:\\大创推荐算法\\cfaProj\\data\\features_sample.csv"
-# PROJECT_PATH = "D:\\压缩包文件\\12-大创"
-# DATA_PATH = "D:\\压缩包文件\\12-大创\\Courses-Recommendation-system-master (2)\\data\\features_sample.csv"
+# PROJECT_PATH = "E:\\大创推荐算法\\cfaProj"
+# DATA_PATH = "E:\\大创推荐算法\\cfaProj\\data\\features_sample.csv"
+PROJECT_PATH = "D:\\压缩包文件\\12-大创\\Demo"
+DATA_PATH = "D:\\压缩包文件\\12-大创\\Demo\\cfaProj\\data\\features_sample.csv"
 
